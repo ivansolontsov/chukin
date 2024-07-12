4775e972572cdf28bbc395bf0e1e8a3de407ca83
Ivan Vasil <ivansolontsov@yandex.ru>
2024-07-12 17:00:28 +0400

    added example of next js

---
+++ frontend/app/page.tsx
@@ -0,0 +1,5 @@
+import Main from "@/source/components/pages/Main/Main";
+
+export default function Page() {
+  return <Main />;
+}
---
+++ frontend/source/components/pages/Main/Main.tsx
@@ -0,0 +1,10 @@
+import s from "./Main.module.scss";
+import { type FC, memo } from "react";
+
+interface MainProps {}
+
+const Main: FC<MainProps> = ({}) => {
+  return <div className={s.yourClassName}>это контент главной страницы</div>;
+};
+
+export default memo(Main);
