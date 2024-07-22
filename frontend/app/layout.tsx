4c785775ef995a0aebea9d8210c056c42e0c6374
ssspelare <spelare.wow.off@gmail.com>
2024-07-22 14:35:04 +0300

    ads

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ua">
+      
       <body className={inter.className}>
         <Header /> 
         {children}
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import {Swiper_back} from "@/source/components/swiper_back";
+import { Swiper_back } from "@/source/components/swiper_back";
 import Image from "next/image";
 import Script from 'next/script'
 
@@ -73,7 +73,80 @@ export default function Home() {
           </div>
         </li>
       </ul>
+      <div className="aboutorder_conatainer_main">
+        <div className="aboutorder_container_item_v1">
+          <p className="aboutorder_container_text">Крок 1.  Надаємо якісну консультацію згідно ваших побажань, для того що ви розуміли з чим ви маєте справу та що вам обрати. (треба більше слів)
+          </p>
+          <div className="aboutorder_img_container">
+            <Image
+              className="aboutorder_img"
+              src="/steps/step1.jpg"
+              alt=""
+              width={900}
+              height={600}
+            /></div>
+        </div>
+        <div className="aboutorder_container_item_v2">
+        <div className="aboutorder_img_container">
+            <Image
+              className="aboutorder_img"
+              src="/steps/step1.jpg"
+              alt=""
+              width={900}
+              height={600}
+            /></div>
+          <p className="aboutorder_container_text">Крок 1.  Надаємо якісну консультацію згідно ваших побажань, для того що ви розуміли з чим ви маєте справу та що вам обрати. (треба більше слів)
+          </p>
+          
+        </div>
+      </div>
+      <div className="a_imformation_container_main">
+        <h2 className="a_imformation_title">ВАЖЛИВА ІНФОРМАЦІЯ</h2>
+        <div className="a_information_items_container">
+          <div className="a_information_item">
+            <div className="a_information_ing_container">
+            <Image
+              className="a_information_img"
+              src="/steps/step1.jpg"
+              alt=""
+              width={900}
+              height={600}
+              />
+            </div>
+            <span className="a_information_item_text">З ЯКИХ МАТЕРІАЛІВ ВИГОТОВЛЯЮТЬСЯ МЕБЛІ RL-MEBEL</span>
+          </div>
+          <div className="a_information_item">
+            <div className="a_information_ing_container">
+            <Image
+              className="a_information_img"
+              src="/steps/step1.jpg"
+              alt=""
+              width={900}
+              height={600}
+              />
+            </div>
+            <span className="a_information_item_text">З ЯКИХ МАТЕРІАЛІВ ВИГОТОВЛЯЮТЬСЯ МЕБЛІ RL-MEBEL</span>
+          </div>
+          <div className="a_information_item">
+            <div className="a_information_ing_container">
+            <Image
+              className="a_information_img"
+              src="/steps/step1.jpg"
+              alt=""
+              width={900}
+              height={600}
+              />
+            </div>
+            <span className="a_information_item_text">З ЯКИХ МАТЕРІАЛІВ ВИГОТОВЛЯЮТЬСЯ МЕБЛІ RL-MEBEL</span>
+          </div>
+        </div>
+        <p className="a_information_item_text_bottom">Ми створюємо унікальний стиль за допомогою якісних меблів. Повний цикл виготовлення меблів в Києві та Закарпатській області під замовлення і в строк!</p>
+
+
+      </div>
       
+      
+
     </main>
 
 
