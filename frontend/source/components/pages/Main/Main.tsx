0413dee11e82b377ca9dc3752b9e17befc5cc87c
Ivan Vasil <ivansolontsov@yandex.ru>
2024-07-31 19:52:04 +0400

    icon example

--- frontend/source/components/pages/Main/Main.tsx
+++ frontend/source/components/pages/Main/Main.tsx
@@ -1,10 +1,15 @@
 import s from "./Main.module.scss";
 import { type FC, memo } from "react";
+import { IconFacebook } from "@/source/components/icons/ExampleIcon";
 
 interface MainProps {}
 
 const Main: FC<MainProps> = ({}) => {
-  return <div className={s.yourClassName}>fdas</div>;
+  return (
+    <div className={s.yourClassName}>
+      <IconFacebook className={s.yourClassNameForIcon} />
+    </div>
+  );
 };
 
 export default memo(Main);
